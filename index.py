46f6651aa1f10a827b2f35f459e79d6c590fd4f1
jaymwanda4-tech <jaymwanda4@gmail.com>
2025-09-12 12:15:51 +0200

    Add files via upload
    These are two different Programmes

---
+++ index.py
@@ -0,0 +1,13 @@
+name = input("Hello, Whats your name?")
+
+print ("Hey", name, "I'm Chat bot AI")
+
+feeling = input("How are you feeling today?")
+if feeling == ("Good"or"Well"):
+    print("Cool!"+ name +"Keep on enjoying your day:)")
+
+elif feeling == ("bad"or"not well") :
+    print ("Im Sorry" + name + "youre gonna be okay!")
+
+else:
+    print("Thank you for your time!")
